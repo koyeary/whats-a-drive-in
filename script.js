f49908b2fb23d29ea9a23620b1631ecaf6c1c0c1
Kat Yeary <katyeary@gmail.com>
2020-04-23 20:12:54 -0700

    style.css

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ function startTime() {
   setTime(secondsLeft); 
   } 
 
-startBtn.addEventListener("click", startTime);
+/* startBtn.addEventListener("click", startTime); */
 
 
 
@@ -110,31 +110,22 @@ function userScore() {
   score.textContent = scoreCount;
 }
 
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-//append questions to modal
-
-/* // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+function myMove() {
+  var elem = document.getElementById("myAnimation");
+  var pos = 0;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos++;
+      elem.style.top = pos + 'px';
+      elem.style.left = pos + 'px';
+    }
   }
-} */
+}
+
+window.addEventListener('load', 
+  function() { 
+    alert('hello!');
+  }, false);
