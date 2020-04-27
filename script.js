7e59b65d773a0925bebb43cea8bc34d91933f50b
Kat Yeary <katyeary@gmail.com>
2020-04-27 12:02:51 -0700

    index.html

--- script.js
+++ script.js
@@ -1,48 +1,96 @@
-var questions = [
-  {
-    q: "Who is the Queen of England?\ a: Elizabeth\ b:Kody Ko\ c: Martha Kelly",
-    answer: "a"
-  },
-  {
-    q: "who is Lauren Rantala?\na: my wife\nb: my fiance\nd: the prime minister of finland",
-    answer: "a"
-  }
-]
+
+
+
 var startBtn = document.querySelector("#startBtn");
 var leaderBtn = document.querySelector("#leaderBtn");
 var timeEl = document.querySelector("#time");
 var scoreEl = document.querySelector("#score"); 
-var secondsLeft = questions.length * 15;
+var secondsLeft = 60;
 var score = 0;
 
 
 var quizContainer = document.querySelector("#quiz");
 var timerInterval;
-
-//an on-click function @ gameOver modal that takes us to the leaderboard
-
-  
-function startQuestions() {
+/* function startQuestions() {
   for (var i = 0; i < questions.length; i++) {
-    var modalQuestion = questions[i].q;
-    var computerAnswer = questions[i].answer;
     vex.dialog.prompt({
-      message: modalQuestion,
-      placeholder: 'Enter a, b, or c',
+      message: questions[i].message,
+      placeholder: questions[i].options,
       callback: function (value) {
-        if (value === computerAnswer) {
-          vex.dialog.alert("You were right! Score +1");
-          userScore();
-        } else if (value != computerAnswer) {
-          vex.dialog.alert("You were wrong! 15 seconds off the clock!");
-           console.log("penalty");
-           userPenalty();
+       console.log(value);
+       return value;
+      }
+    
+    });
+    
+  } */
+
+/* function startQuestions() {
+  for (var i = 0; i < questions.length; i++) {
+    x = questions[i];
+    questionLoop(x);
+    if (myAnswer === )
+    }
+  }
+
+
+  function questionLoop() {
+  /*   for (var i = 0; i < questions.length; i++) { 
+      var myAnswer = vex.dialog.prompt({
+        message: x.message,
+        placeholder: x.options,
+        callback: function (value) {
+          console.log(value);
+          return value;
         }
+      });
+    } */
+var questions =  [{
+      message: "Who is Brenda?",
+      options: "a: a lady, b: a dog, or c: the president",
+      answer: "blah",
+    },
+    
+  {
+      message: "Who is Joanna",
+      options: "testing",
+      answer: "a",
+    },
+    
+  {
+      message: "Who is Laura",
+      options: "where does it break",
+      answer: "b",
+    },
+    
+    {
+      message: "Who is Ashley?",
+      options: "who can we blame",
+      answer: "c",
+    }];
+
+
+function startQuestions() {
+  console.log(questions.length);
+  for (var i = 0; i < questions.length; i++) {
+    var y = questions[i];
+    vex.dialog.prompt({
+      message: questions[i].message,
+      placeholder: questions[i].options,
+      callback: function (value) {
+        checkAnswer(value);
       }
     });
+  function checkAnswer(x, y){
+  if (x === y) {
+      console.log("okay");
+     }}
+    
   }
-/*   endGame(); */
 }
+
+
+
 
 //need a variable that stores the new number of seconds
 //this is decremented by 30 and then is set as the secondsLeft variable
@@ -57,10 +105,9 @@ function setTime() {
     console.log(secondsLeft);
     //Time is up when timer reaches zero. 
     if (secondsLeft < 1) {
-      /*  endGame();} */
-      vex.dialog.alert("Game Over!")
-      timeEl.textContent = 0;
-      clearInterval(timerInterval);
+      endGame();
+     /*  timeEl.textContent = 0;
+      clearInterval(timerInterval); */
     }
     // 1000 milliseconds equals 1 second, so our timer counts down by 1 second
   },
@@ -90,27 +137,22 @@ function userPenalty() {
 }
  
 function endGame() {
+  timeEl.textContent = 0;
   clearInterval(timerInterval);
   vex.dialog.prompt({
-    message: "You scored" + score,
+    message: "You scored " + score + " points!",
     placeholder: "Enter your initials and go to the leaderboard!",
     callback: function (value) {
-      timeEl.textContent = 0;
+      var userInfo = {
+        User: value,
+        Score: score,
+      }
+      window.localStorage.setItem('user', JSON.stringify(userInfo));
+      console.log(window.localStorage.getItem('user'));
+      //open new window to leaderboard or new modal to leaderboard
     }
-  })
-}
-
-/*Starts the countdown with a 1 second delay for the quiz to begin,
-so the time element starts to update without being delayed by the first modal*/
-/* function hitStart() { */
- /*  setTimeout(() => {startQuestions(); }, 1000);
-}
- */
+  });
+} 
 /* wrongAnswer.addEventListener("Wrong Answer", wrongEventHandler, true); */
 /* leaderBtn.addEventListener("click", openLeaderboard); */
 startBtn.addEventListener("click", startTime);
-
-/* function openLeaderboard {
-  
-}
- */
